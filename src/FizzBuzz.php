6dca6378bc005586d7ca5497298509a23e35df2b
Mariusz Kosek <mdziegielowski@gmail.com>
2021-03-12 15:37:11 +0000

    Example solution

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -4,5 +4,39 @@ namespace App;
 
 class FizzBuzz
 {
+    /**
+     * @param int $number
+     * @return string
+     */
+    public function process(int $number): string
+    {
+        $response = '';
 
+        foreach (range(1, $number) as $item) {
+            $response .= $this->format($item) . "\n";
+        }
+
+        return trim($response);
+    }
+
+    /**
+     * @param int $number
+     * @return string
+     */
+    public function format(int $number): string
+    {
+        if ($number % 15 === 0) {
+            return 'FizzBuzz';
+        }
+
+        if ($number % 3 === 0) {
+            return 'Fizz';
+        }
+
+        if ($number % 5 === 0) {
+            return 'Buzz';
+        }
+
+        return $number;
+    }
 }
--- tests/Unit/FizzBuzzTest.php
+++ tests/Unit/FizzBuzzTest.php
@@ -2,9 +2,74 @@
 
 namespace Tests\Unit;
 
+use App\FizzBuzz;
 use PHPUnit\Framework\TestCase;
 
 class FizzBuzzTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function itFormatsIntegersAsStrings()
+    {
+        $fizzBuzz = new FizzBuzz();
 
+        $this->assertSame('1', $fizzBuzz->format(1));
+        $this->assertSame('7', $fizzBuzz->format(7));
+        $this->assertSame('22', $fizzBuzz->format(22));
+    }
+
+    /**
+     * @test
+     */
+    public function itReturnsMultiplesOf3AsFizz()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $this->assertSame('Fizz', $fizzBuzz->format(3));
+    }
+
+    /**
+     * @test
+     */
+    public function itReturnsMultiplesOf5AsBuzz()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $this->assertSame('Buzz', $fizzBuzz->format(5));
+    }
+
+    /**
+     * @test
+     */
+    public function itReturnsMultiplesOf15AsFizzBuzz()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $this->assertSame('FizzBuzz', $fizzBuzz->format(15));
+    }
+
+    /**
+     * @test
+     */
+    public function itCanProcessARangeOfNumbers()
+    {
+        $fizzBuzz = new FizzBuzz();
+
+        $this->assertSame('1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz', $fizzBuzz->process(15));
+    }
 }
